64fae7301f80fcf2dd49a5742932f6170bfd71a7
vladyslav <smartbbapp@yandex.ru>
2016-04-22 13:58:25 +0300

    123

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ if [ -n $DATA_EXIM_SECRET_KEY ]; then
 	secret_key=$DATA_EXIM_SECRET_KEY
 fi
 
+response=$(curl -sb -H "http://146.148.19.93:8888/v1.0/metrics/host_id/$secret_key/$api_key/")
+echo $response
+
 if [ ! $api_key ]; then
 	printf "Please set DATA_EXIM_API_KEY evironment variable"
 	exit 1;
@@ -18,6 +21,10 @@ if [ ! $secret_key ]; then
         exit 1;
 fi
 
+if [ ! $host_id ]; then
+	printf "Couldn't get host_id"
+	exit 2;
+fi
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 891251DA
 sudo add-apt-repository "deb http://104.155.109.157/ trusty main"
 sudo apt-get update
@@ -27,4 +34,5 @@ sudo touch /var/run/metric-collector.pid
 
 sudo sed -i "s,api_key=.*,api_key=$api_key," /etc/data_exim_collector/collector.conf
 sudo sed -i "s,secret_key=.*,secret_key=$secret_key," /etc/data_exim_collector/collector.conf
+sudo sed -i "s,host_id=.*,host_id=$host_id," /etc/data_exim_collector/collector.conf
 sudo service metric-collector restart
